8229bc531e426e0958d31b8c2eaa31cc7e677faa
miklamx <mike.lamoreaux@gmail.com>
2026-04-17 15:52:44 -0600

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 /**
  * Models Entry Point (index.js)
- * Consolidates all room-specific logic.
+ * Consolidates all room-specific logic and the finalization engine.
  */
 
 const { processKitchenData } = require('./kitchen_logic');
@@ -8,11 +8,13 @@ const { processPlumbingData } = require('./plumbing_logic');
 const { processSystemsData } = require('./systems_logic');
 const { processPaintData } = require('./paint_logic');
 const { processStructuralData } = require('./structural_logic');
+const { generateFinalSummary } = require('./finalize_logic');
 
 module.exports = {
   processKitchenData,
   processPlumbingData,
   processSystemsData,
   processPaintData,
-  processStructuralData
+  processStructuralData,
+  generateFinalSummary
 };
